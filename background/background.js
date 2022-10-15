c76e4fb7564d8b24d5977fdf6e1ab497662f6158
godtaehee <godtaeheeDev@gmail.com>
2022-10-16 00:55:49 +0900

    Add Token, Channel

--- background/background.js
+++ background/background.js
@@ -1,100 +1,103 @@
-const GOOGLE_SPEECH_URI = 'https://www.google.com/speech-api/v1/synthesize',
-
-    DEFAULT_HISTORY_SETTING = {
-        enabled: true
-    };
+const GOOGLE_SPEECH_URI = "https://www.google.com/speech-api/v1/synthesize",
+  DEFAULT_HISTORY_SETTING = {
+    enabled: true,
+  };
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    const { word, lang } = request,
-        url = `https://www.google.com/search?hl=${lang}&q=define+${word}&gl=US`;
+  const { word, lang } = request,
+    url = `https://www.google.com/search?hl=${lang}&q=define+${word}&gl=US`;
 
+  fetch(url, {
+    method: "GET",
+    credentials: "omit",
+  })
+    .then((response) => response.text())
+    .then((text) => {
+      const document = new DOMParser().parseFromString(text, "text/html"),
+        content = extractMeaning(document, { word, lang });
 
-    fetch(url, { 
-            method: 'GET',
-            credentials: 'omit'
-        })
-        .then((response) => response.text())
-        .then((text) => {
-            const document = new DOMParser().parseFromString(text, 'text/html'),
-                content = extractMeaning(document, { word, lang });
+      sendResponse({ content });
 
-            sendResponse({ content });
+      content &&
+        browser.storage.local.get().then((results) => {
+          let history = results.history || DEFAULT_HISTORY_SETTING;
 
-            content && browser.storage.local.get().then((results) => {
-                let history = results.history || DEFAULT_HISTORY_SETTING;
+          history.enabled && saveWord(content);
+        });
+    });
 
-                history.enabled && saveWord(content)
-            });
-        })
-
-
-
-    return true;
+  return true;
 });
 
-function extractMeaning (document, context) {
+function extractMeaning(document, context) {
+  if (!document.querySelector("[data-dobid='hdw']")) {
+    return null;
+  }
 
-    if (!document.querySelector("[data-dobid='hdw']")) { return null; }
-    
-    var word = document.querySelector("[data-dobid='hdw']").textContent,
-        definitionDiv = document.querySelector("div[data-dobid='dfn']"),
-        meaning = "";
+  var word = document.querySelector("[data-dobid='hdw']").textContent,
+    definitionDiv = document.querySelector("div[data-dobid='dfn']"),
+    meaning = "";
 
-    if (definitionDiv) {
-        definitionDiv.querySelectorAll("span").forEach(function(span){
-            if(!span.querySelector("sup"))
-                 meaning = meaning + span.textContent;
-        });
+  if (definitionDiv) {
+    definitionDiv.querySelectorAll("span").forEach(function (span) {
+      if (!span.querySelector("sup")) meaning = meaning + span.textContent;
+    });
+  }
+
+  meaning = meaning[0].toUpperCase() + meaning.substring(1);
+
+  var audio = document.querySelector("audio[jsname='QInZvb']"),
+    source = document.querySelector("audio[jsname='QInZvb'] source"),
+    audioSrc = source && source.getAttribute("src");
+
+  if (audioSrc) {
+    !audioSrc.includes("http") &&
+      (audioSrc = audioSrc.replace("//", "https://"));
+  } else if (audio) {
+    let exactWord = word.replace(/·/g, ""), // We do not want syllable seperator to be present.
+      queryString = new URLSearchParams({
+        text: exactWord,
+        enc: "mpeg",
+        lang: context.lang,
+        speed: "0.4",
+        client: "lr-language-tts",
+        use_google_only_voices: 1,
+      }).toString();
+
+    audioSrc = `${GOOGLE_SPEECH_URI}?${queryString}`;
+  }
+
+  return { word: word, meaning: meaning, audioSrc: audioSrc };
+}
+
+function saveWord(content) {
+  let word = content.word,
+    meaning = content.meaning,
+    storageItem = browser.storage.local.get();
+
+  storageItem.then((results) => {
+    let definitions = results.definitions || {};
+    const token = results.authToken || "";
+    const channel = results.channel || "";
+
+    let form_data = new FormData();
+    form_data.append("token", token);
+    form_data.append("channel", channel);
+    form_data.append("text", `*${word}*\n` + `- ${meaning}`);
+
+    if (!definitions[word]) {
+      fetch("https://slack.com/api/chat.postMessage", {
+        mode: "cors",
+        method: "POST",
+        body: form_data,
+      })
+        .then((test) => test.json())
+        .then((data) => data);
     }
 
-    meaning = meaning[0].toUpperCase() + meaning.substring(1);
-
-    var audio = document.querySelector("audio[jsname='QInZvb']"),
-        source = document.querySelector("audio[jsname='QInZvb'] source"),
-        audioSrc = source && source.getAttribute('src');
-
-    if (audioSrc) {
-        !audioSrc.includes("http") && (audioSrc = audioSrc.replace("//", "https://"));
-    }
-    else if (audio) {
-        let exactWord = word.replace(/·/g, ''), // We do not want syllable seperator to be present.
-            
-        queryString = new URLSearchParams({
-            text: exactWord, 
-            enc: 'mpeg', 
-            lang: context.lang, 
-            speed: '0.4', 
-            client: 'lr-language-tts', 
-            use_google_only_voices: 1
-        }).toString();
-
-        audioSrc = `${GOOGLE_SPEECH_URI}?${queryString}`;
-    }
-
-
-
-    return { word: word, meaning: meaning, audioSrc: audioSrc };
-};
-
-function saveWord (content) {
-    let word = content.word,
-        meaning = content.meaning,
-        storageItem = browser.storage.local.get();
-
-        storageItem.then((results) => {
-            let definitions = results.definitions || {};
-            let slackURL = results.slack || 'http://localhost:3000';
-
-            if (!definitions[word]) {
-                fetch(slackURL, {
-                    method: 'POST',
-                    body: JSON.stringify({text: 'test?'})
-                }).then(test => test.json()).then(data => console.log(data))
-            }
-
-            definitions[word] = meaning;
-            browser.storage.local.set({
-                definitions
-            });
-        })
+    definitions[word] = meaning;
+    browser.storage.local.set({
+      definitions,
+    });
+  });
 }
--- options/options.js
+++ options/options.js
@@ -1,137 +1,145 @@
-const DEFAULT_LANGUAGE = 'en',
-    DEFAULT_TRIGGER_KEY = 'none',
-    IS_HISTORY_ENABLED_BY_DEFAULT = true,
-
-    SAVE_STATUS = document.querySelector("#save-status"),
-
-    SAVE_OPTIONS_BUTTON = document.querySelector("#save-btn"),
-    RESET_OPTIONS_BUTTON = document.querySelector("#reset-btn"),
-
-    CLEAR_HISTORY_BUTTON = document.querySelector("#clear-history-btn"),
-    DOWNLOAD_HISTORY_BUTTON = document.querySelector("#download-history-btn"),
-
-    OS_MAC = 'mac',
-
-    KEY_COMMAND = 'Command',
-    KEY_META = 'meta';
-
-
+const DEFAULT_LANGUAGE = "en",
+  DEFAULT_TRIGGER_KEY = "none",
+  IS_HISTORY_ENABLED_BY_DEFAULT = true,
+  SAVE_STATUS = document.querySelector("#save-status"),
+  SAVE_OPTIONS_BUTTON = document.querySelector("#save-btn"),
+  RESET_OPTIONS_BUTTON = document.querySelector("#reset-btn"),
+  CLEAR_HISTORY_BUTTON = document.querySelector("#clear-history-btn"),
+  DOWNLOAD_HISTORY_BUTTON = document.querySelector("#download-history-btn"),
+  OS_MAC = "mac",
+  KEY_COMMAND = "Command",
+  KEY_META = "meta";
 
 function saveOptions(e) {
-    browser.storage.local.set({
-        language: document.querySelector("#language-selector").value,
-        interaction: {
-            dblClick: {
-                key: document.querySelector("#popup-dblclick-key").value
-            }
+  browser.storage.local
+    .set({
+      language: document.querySelector("#language-selector").value,
+      interaction: {
+        dblClick: {
+          key: document.querySelector("#popup-dblclick-key").value,
         },
-        history: {
-            enabled: document.querySelector("#store-history-checkbox").checked
+      },
+      history: {
+        enabled: document.querySelector("#store-history-checkbox").checked,
+      },
+      authToken: document.querySelector("#user-slack-bearer-token").value,
+      channel: document.querySelector("#slack-channel").value,
+    })
+    .then(showSaveStatusAnimation);
+
+  e.preventDefault();
+}
+
+function restoreOptions() {
+  let storageItem = browser.storage.local.get();
+
+  storageItem.then((results) => {
+    let language = results.language,
+      interaction = results.interaction || {},
+      history = results.history || { enabled: IS_HISTORY_ENABLED_BY_DEFAULT },
+      definitions = results.definitions || {},
+      authToken = results.authToken,
+      channel = results.channel;
+
+    // language
+    document.querySelector("#language-selector").value =
+      language || DEFAULT_LANGUAGE;
+
+    // interaction
+    // document.querySelector("#popup-dblclick-checkbox").checked = interaction.dblClick.enabled;
+    document.querySelector("#popup-dblclick-key").value =
+      (interaction.dblClick && interaction.dblClick.key) || DEFAULT_TRIGGER_KEY;
+
+    // document.querySelector("#popup-select-checkbox").checked = interaction.select.enabled;
+    // document.querySelector("#popup-select-key").value = interaction.select.key;
+
+    // history
+    document.querySelector("#store-history-checkbox").checked = history.enabled;
+    document.querySelector("#num-words-in-history").innerText =
+      Object.keys(definitions).length;
+
+    document.querySelector("#user-slack-bearer-token").value = authToken;
+    document.querySelector("#slack-channel").value = channel;
+  });
+}
+
+function downloadHistory(e) {
+  let fileContent = "";
+  (storageItem = browser.storage.local.get("definitions")),
+    (anchorTag = document.querySelector("#download-history-link"));
+
+  storageItem.then((results) => {
+    let definitions = results.definitions || {};
+
+    for (definition in definitions) {
+      if (!definitions.hasOwnProperty(definition)) {
+        return;
+      }
+
+      fileContent += definition;
+      fileContent += "\t";
+      fileContent += "\t";
+      fileContent += definitions[definition];
+      fileContent += "\n";
+    }
+
+    anchorTag.href = window.URL.createObjectURL(
+      new Blob([fileContent], {
+        type: "text/plain",
+      })
+    );
+
+    anchorTag.dispatchEvent(new MouseEvent("click"));
+  });
+
+  e.preventDefault();
+}
+
+function resetOptions(e) {
+  browser.storage.local
+    .set({
+      language: DEFAULT_LANGUAGE,
+      interaction: {
+        dblClick: {
+          key: DEFAULT_TRIGGER_KEY,
         },
-        slack: document.querySelector("#user-slack-url").value
-    }).then(showSaveStatusAnimation);
+      },
+      history: {
+        enabled: IS_HISTORY_ENABLED_BY_DEFAULT,
+      },
+    })
+    .then(restoreOptions);
 
-    e.preventDefault();
-  }
-  
-  function restoreOptions() {
-    let storageItem = browser.storage.local.get();
+  e.preventDefault();
+}
 
-    storageItem.then((results) => {
-        let language = results.language,
-            interaction = results.interaction || {},
-            history = results.history || { enabled: IS_HISTORY_ENABLED_BY_DEFAULT },
-            definitions = results.definitions || {},
-            slackURL = results.slack;
-        
-        // language
-        document.querySelector("#language-selector").value = language || DEFAULT_LANGUAGE;
+function clearHistory(e) {
+  browser.storage.local.set({ definitions: {} });
 
-        // interaction
-        // document.querySelector("#popup-dblclick-checkbox").checked = interaction.dblClick.enabled;
-        document.querySelector("#popup-dblclick-key").value = (interaction.dblClick && interaction.dblClick.key) || DEFAULT_TRIGGER_KEY;
-        
-        // document.querySelector("#popup-select-checkbox").checked = interaction.select.enabled;
-        // document.querySelector("#popup-select-key").value = interaction.select.key;
+  e.preventDefault();
+}
 
-        // history
-        document.querySelector("#store-history-checkbox").checked = history.enabled;
-        document.querySelector("#num-words-in-history").innerText = Object.keys(definitions).length;
+function showSaveStatusAnimation() {
+  SAVE_STATUS.style.setProperty("-webkit-transition", "opacity 0s ease-out");
+  SAVE_STATUS.style.opacity = 1;
+  window.setTimeout(function () {
+    SAVE_STATUS.style.setProperty(
+      "-webkit-transition",
+      "opacity 0.4s ease-out"
+    );
+    SAVE_STATUS.style.opacity = 0;
+  }, 1500);
+}
 
-      document.querySelector("#user-slack-url").value = slackURL;
+document.addEventListener("DOMContentLoaded", restoreOptions);
 
-    });
-  }
-  
-  function downloadHistory (e) {
-    let fileContent = "" 
-        storageItem = browser.storage.local.get("definitions"),
-        anchorTag = document.querySelector("#download-history-link");
+CLEAR_HISTORY_BUTTON.addEventListener("click", clearHistory);
+DOWNLOAD_HISTORY_BUTTON.addEventListener("click", downloadHistory);
 
-    storageItem.then((results) => {
-        let definitions = results.definitions || {};
+SAVE_OPTIONS_BUTTON.addEventListener("click", saveOptions);
+RESET_OPTIONS_BUTTON.addEventListener("click", resetOptions);
 
-        for (definition in definitions) {
-            if (!definitions.hasOwnProperty(definition)) { return; }
-
-            fileContent += definition;
-            fileContent += "\t";
-            fileContent += "\t";
-            fileContent += definitions[definition];
-            fileContent += "\n";
-        }
-
-        anchorTag.href = window.URL.createObjectURL(new Blob([fileContent],{
-            type: "text/plain"
-        }));
-
-        anchorTag.dispatchEvent(new MouseEvent('click'));
-    });
-
-    e.preventDefault();
-  }
-
-  function resetOptions (e) {
-    browser.storage.local.set({
-        language: DEFAULT_LANGUAGE,
-        interaction: {
-            dblClick: {
-                key: DEFAULT_TRIGGER_KEY
-            }
-        },
-        history: {
-            enabled: IS_HISTORY_ENABLED_BY_DEFAULT
-        }
-    }).then(restoreOptions);
-
-    e.preventDefault();
-  }
-
-  function clearHistory(e) {
-    browser.storage.local.set({ definitions: {} });
-
-    e.preventDefault();
-  }
-
-  function showSaveStatusAnimation () {
-    SAVE_STATUS.style.setProperty("-webkit-transition", "opacity 0s ease-out");
-    SAVE_STATUS.style.opacity = 1;
-    window.setTimeout(function() {
-        SAVE_STATUS.style.setProperty("-webkit-transition", "opacity 0.4s ease-out");
-        SAVE_STATUS.style.opacity = 0
-    }, 1500);
-  }
-
-  document.addEventListener('DOMContentLoaded', restoreOptions);
-
-  CLEAR_HISTORY_BUTTON.addEventListener("click", clearHistory);
-  DOWNLOAD_HISTORY_BUTTON.addEventListener("click", downloadHistory);
-
-  SAVE_OPTIONS_BUTTON.addEventListener("click", saveOptions);
-  RESET_OPTIONS_BUTTON.addEventListener("click", resetOptions);
-
-  if (window.navigator.platform.toLowerCase().includes(OS_MAC)) {
-    document.getElementById("popup-dblclick-key-ctrl").textContent = KEY_COMMAND;
-    document.getElementById("popup-dblclick-key-ctrl").value = KEY_META;
-  }
-
+if (window.navigator.platform.toLowerCase().includes(OS_MAC)) {
+  document.getElementById("popup-dblclick-key-ctrl").textContent = KEY_COMMAND;
+  document.getElementById("popup-dblclick-key-ctrl").value = KEY_META;
+}
